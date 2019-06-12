a03fed92615ff61a7364f8faf99dc901f36eba68
Liam Daley <39020721+daleylp@users.noreply.github.com>
2019-06-12 18:14:37 +1200

    Checking for prime numbers correctly

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-
 def get_hit_count():
     retries = 5
     while True:
@@ -18,8 +17,44 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+# TODO Check if the number is in redis
+def in_redis(number):
+    return False
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+# TODO Store the number in redis
+def store_number_in_redis(number):
+    return "stored the number"
+
+# Check if number is prime
+def is_prime_number(number):
+    #if in_redis(number):
+    #   return true
+    # Check if prime number
+    #sys.maxint
+    if number > 1:
+        for i in range(2,number):
+            if (number % i) == 0:
+                return False;
+                break
+            else:
+                #store_number_in_redis(number)
+                return True;
+    else:
+        return False
+    
+
+# Checks if number is prime and returns appropriate message
+@app.route('/isPrime/<number>')
+def check_number(number):
+    #try:
+    number = int(number)
+    if is_prime_number(number):
+        return "%d is prime" % number
+    return "%d is not prime" % number
+    #except:
+        #pass
+
+# TODO Returns a list with all the primes stored in the connected Redis service
+@app.route('/primesStored')
+def get_prime_numbers():
+    return "all prime numbers"
